5c7f6c9117c33e40120673eb9ab3c1d3d80f9bda
Andreas Johansson <ajoh@hms.se>
2024-12-18 19:14:31 +0100

    Example for attributes lookup to handle SET commands.

--- src/example_application/impl_attr_cfb.c
+++ src/example_application/impl_attr_cfb.c
@@ -4,12 +4,12 @@
 ** Callback functions used by attribute table.
 ** **------------------------------------------------------------------------------
 */
-UINT32 ABCC_ImplApplicationObjSerialNum(void)
+UINT32 ABCC_ImplApplicationObjSerialNum_Get(void)
 {
     return( 0xFF000001 );
 }
 
-UINT16 ABCC_ImplApplicationObjVendortName( char* pPackedStrDest, UINT16 iBuffSize )
+UINT16 ABCC_ImplApplicationObjVendortName_Get( char* pPackedStrDest, UINT16 iBuffSize )
 {
    static const char* my_product_name = "My dynamic vendor name";
    UINT16 iStrLength = (UINT16)strlen(my_product_name);
@@ -19,7 +19,7 @@ UINT16 ABCC_ImplApplicationObjVendortName( char* pPackedStrDest, UINT16 iBuffSiz
    return( iStrLength );
 }
 
-UINT16 ABCC_ImplApplicationObjProductName( char* pPackedStrDest, UINT16 iBuffSize )
+UINT16 ABCC_ImplApplicationObjProductName_Get( char* pPackedStrDest, UINT16 iBuffSize )
 {
    static const char* my_product_name = "My dynamic product name";
    UINT16 iStrLength = (UINT16)strlen(my_product_name);
@@ -29,22 +29,33 @@ UINT16 ABCC_ImplApplicationObjProductName( char* pPackedStrDest, UINT16 iBuffSiz
    return( iStrLength );
 }
 
+void ABCC_ImplApplicationObjFirmwareAvailable_Set( UINT8 bValue )
+{
+    /* Store FW flag in NVS. */
+    ABCC_LOG_INFO( "Candidate FW flag: %u\n", bValue );
+}
+
+UINT8 ABCC_ImplApplicationObjFirmwareAvailable_Get( void )
+{
+   /* Return FW flag from NVS. */
+   return( 0 );
+}
 
 
 
 
 
-UINT16 ABCC_ImplEthernetIpObjVendorId( void )
+UINT16 ABCC_ImplEthernetIpObjVendorId_Get( void )
 {
    return( 1133 );
 }
 
-UINT16 ABCC_ImplEthernetIpObjDeviceType( void )
+UINT16 ABCC_ImplEthernetIpObjDeviceType_Get( void )
 {
    return( 6655 );
 }
 
-UINT16 ABCC_ImplEthernetIpObjProductCode( void )
+UINT16 ABCC_ImplEthernetIpObjProductCode_Get( void )
 {
    return( 911 );
 }
@@ -54,17 +65,17 @@ UINT16 ABCC_ImplEthernetIpObjProductCode( void )
 
 
 
-UINT16 ABCC_ImplProfinetIoObjDeviceId( void )
+UINT16 ABCC_ImplProfinetIoObjDeviceId_Get( void )
 {
    return( 0xBEEF );
 }
 
-UINT16 ABCC_ImplProfinetIoObjVendorId( void )
+UINT16 ABCC_ImplProfinetIoObjVendorId_Get( void )
 {
    return( 0xDEAD );
 }
 
-UINT16 ABCC_ImplProfinetIoObjOrderId( char* pPackedStrDest, UINT16 iBuffSize )
+UINT16 ABCC_ImplProfinetIoObjOrderId_Get( char* pPackedStrDest, UINT16 iBuffSize )
 {
    static const char* my_product_name = "My dynamic OrderId";
    UINT16 iStrLength = (UINT16)strlen(my_product_name);
